a3ebd1417009cc62ac37dd142658a502eeb46a95
yuto ichinose <yuto.0526.sano@gmail.com>
2024-05-03 12:42:30 +0900

    feat: 選択年度と出題数の同期

--- app/src/components/features/question/question_years.tsx
+++ app/src/components/features/question/question_years.tsx
@@ -27,13 +27,32 @@ export const QuestionYears: FC<QuestionYearsProps> = ({ questions_years }) => {
   const [selectedValues, setSelectedValues] = useState<string[]>([]);
   const [type, setType] = useState<string>('sequential');
   const [questionLength, setQuestionLength] = useState<number>(0);
+  const totalQuestions = questions_years.reduce((accumulator, currentValue) => accumulator + currentValue.questionsCount, 0);
 
-
+  const handleChange = (value: string[]) => {
+    setSelectedValues(value);
+    if (value.length > 0) {
+      let newCount = 0;
+      questions_years.forEach(v => {
+        if (value.includes(v.questionYearId.toString())) {
+          newCount += v.questionsCount;
+        }
+      });
+      console.log(newCount);
+      
+      setQuestionLength(newCount);
+      return;
+    }
+    setQuestionLength(0);
+  };
+  
   const handleAllClick = () => {
-    if (selectedValues.length === questions_years.length) {
+    if (selectedValues.length === questions_years.filter(e => e.questionsCount !== 0).length) {
       setSelectedValues([]);
+      setQuestionLength(0);
     } else {
-      setSelectedValues(questions_years.map(e => e.questionYearId.toString()));
+      setSelectedValues(questions_years.filter(e => e.questionsCount !== 0).map(e => e.questionYearId.toString()));
+      setQuestionLength(totalQuestions);
     }
   };
 
@@ -52,9 +71,9 @@ export const QuestionYears: FC<QuestionYearsProps> = ({ questions_years }) => {
         <Loading size="5xl" />
       </Center> : <Container>
         <Heading m="auto">問題選択</Heading>
-        <CheckboxGroup value={selectedValues} flexDir="row" flexWrap="wrap" onChange={setSelectedValues}>
+        <CheckboxGroup value={selectedValues} flexDir="row" flexWrap="wrap" onChange={handleChange}>
           {questions_years.map((question_year, index) => (
-            <CustomCheckbox key={`${index}-${question_year.questionYearId}`} createdYearJp={question_year.createdYearJp} questionYearId={question_year.questionYearId.toString()} season={question_year.season} />
+            <CustomCheckbox key={`${index}-${question_year.questionYearId}`} questionCount={question_year.questionsCount} createdYearJp={question_year.createdYearJp} questionYearId={question_year.questionYearId.toString()} season={question_year.season} />
           ))}
         </CheckboxGroup>
         <Box px="sm">
@@ -68,12 +87,12 @@ export const QuestionYears: FC<QuestionYearsProps> = ({ questions_years }) => {
           </RadioGroup>
           <HStack>
             <Text>出題数</Text>
-            <NumberInput min={0} defaultValue={5} value={questionLength} onChange={value => setQuestionLength(parseInt(value))} />
+            <NumberInput min={0} max={totalQuestions} defaultValue={5} value={questionLength} onChange={value => setQuestionLength(value === '' ? 0 : parseInt(value))} />
             <Text>問</Text>
           </HStack>
         </HStack>
         <Center>
-          <Button colorScheme='success' isDisabled={!selectedValues.length}>出題開始</Button>
+          <Button colorScheme='success' isDisabled={selectedValues.length === 0 || questionLength === 0}>出題開始</Button>
         </Center>
       </Container>
       }
@@ -81,13 +100,13 @@ export const QuestionYears: FC<QuestionYearsProps> = ({ questions_years }) => {
   );
 };
 
-const CustomCheckbox: FC<{ createdYearJp: string; season: string; questionYearId: string; }> = ({ createdYearJp, questionYearId, season }) => {
+const CustomCheckbox: FC<{ createdYearJp: string; season: string; questionYearId: string; questionCount: number; }> = ({ createdYearJp, questionYearId, season, questionCount }) => {
   const {
     onPointerDown,
     ...rippleProps
   } = useRipple();
-  return <Checkbox position="relative" overflow="hidden" w="36" m="sm" p="sm" border="1px solid black" borderRadius="md" value={`${questionYearId}`} onPointerDown={onPointerDown}>
-    <Box >
+  return <Checkbox position="relative" overflow="hidden" w="36" m="sm" p="sm" border="1px solid black" borderRadius="md" value={`${questionYearId}`} isDisabled={!questionCount} onPointerDown={onPointerDown}>
+    <Box>
       {`${createdYearJp} ${season}`}
       <Ripple {...rippleProps} />
     </Box>
--- app/src/lib/question/index.ts
+++ app/src/lib/question/index.ts
@@ -5,21 +5,37 @@ export const getQuestionsYears = async () => {
   let connection;
   try {
     connection = await mysql_connection();
-    const query =
-      'SELECT question_year_id, created_year_jp, created_year, season FROM past_questions_years';
+    const query = `SELECT 
+      pqy.question_year_id, 
+      pqy.created_year_jp, 
+      pqy.created_year, 
+      pqy.season, 
+      COUNT(pq.question_id) AS past_questions_count
+  FROM 
+      past_questions_years pqy
+  LEFT JOIN 
+      past_questions pq ON pqy.question_year_id = pq.question_year_id
+  GROUP BY 
+      pqy.question_year_id, 
+      pqy.created_year_jp, 
+      pqy.created_year, 
+      pqy.season;
+  `;
     const [result] = (await connection.execute(query)) as RowDataPacket[];
-    const questionsYears =  (
+    const questionsYears = (
       result as {
         question_year_id: number;
         created_year_jp: string;
         created_year: number;
         season: string;
+        past_questions_count: number;
       }[]
     ).map((questionYear) => ({
       questionYearId: questionYear.question_year_id,
       createdYearJp: questionYear.created_year_jp,
       createdYear: questionYear.created_year,
       season: questionYear.season,
+      questionsCount: questionYear.past_questions_count
     }));
 
     return questionsYears;
